c41e88edac05b0b7a945c48507b3fd8fc26003db
CreativeSona <46821390+CreativeSona@users.noreply.github.com>
2019-11-23 11:12:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var app = angular.module('myAngularApp', []);
+var app = angular.module('myAngularApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "linesorter.html"
+  })
+  .when("/sort", {
+    templateUrl : "sort.html"
+  })
+});
+</script>
 app.controller('mainController', function($scope) {
   $scope.homePageMsg = "Welcome to Basics of CSE!!!";
 });
