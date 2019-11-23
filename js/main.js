76cc6dd4b548a2de597fef33d584b1d8cea516cb
CreativeSona <46821390+CreativeSona@users.noreply.github.com>
2019-11-23 11:26:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ app.config(function($routeProvider) {
     templateUrl : "sort.html"
   })
 });
-</script>
+
 app.controller('mainController', function($scope) {
   $scope.homePageMsg = "Welcome to Basics of CSE!!!";
 });
