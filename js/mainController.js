e39235ae822bfbd0413328a8393b0c1e7d77e6c1
CreativeSona <46821390+CreativeSona@users.noreply.github.com>
2019-11-27 07:27:57 +0100

    Update mainController.js

--- js/mainController.js
+++ js/mainController.js
@@ -1,12 +1,13 @@
 var app = angular.module('myAngularApp', ["ngRoute"]);
 
 app.config(function($routeProvider) {
-  $routeProvider
-  .when("/index", {
-    templateUrl : "linesorter.html"
+  $routeProvider.when("/linesorter.html", {
+    templateUrl : "linesorter.html",
+    controller: "mainController"
   })
   .when("/sort", {
-    templateUrl : "sort.html"
+    templateUrl : "sort.html",
+    controller: "sortController"
   })
 });
 
