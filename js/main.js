9ee576c1ce3167d286027b958841cb2b4b13c7b9
CreativeSona <46821390+CreativeSona@users.noreply.github.com>
2019-12-30 19:35:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,31 @@ app.config(function($routeProvider) {
     controller: "sortController"
   })
 });
+
+app.controller('mainController', function($scope) {
+  $scope.homePageMsg = "Welcome to Basics of CSE!!!";
+});
+app.controller('sortController', function($scope) { alert("hi");
+      $scope.test= "test";
+      $scope.swap = function(num1, num2) {
+        var temp = num1;
+        num1=num2;
+        num2=num1;
+        return [num1, num2];
+      }
+
+      $scope.selectionSort = function() { alert("sort");
+        var Arr = $scope.values;
+        var mn = 0;
+        var len = Arr.length;
+        for(var i =0;i<len-1;i++){
+          mn=i;
+          for(var j=i+1;j<len;j++){
+            if(Arr[j] < Arr[mn])
+              mn=j;
+          }
+          [Arr[i], Arr[mn]]= $scope.swap(Arr[i], Arr[mn]);
+        }
+      }
+  }
+});
