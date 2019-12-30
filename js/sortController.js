684e0cffb00daa4b6f936a09c1210d892fc41a26
CreativeSona <46821390+CreativeSona@users.noreply.github.com>
2019-12-30 19:34:54 +0530

    Update and rename sort1Controller.js to sortController.js

--- js/sortController.js
+++ js/sortController.js
@@ -1,4 +1,4 @@
-app.controller('sortController', function($scope) { alert("hi");
+app.controller('sort1Controller', function($scope) { alert("hi");
       $scope.test= "test";
       $scope.swap = function(num1, num2) {
         var temp = num1;
